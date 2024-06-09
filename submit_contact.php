10e9fe25a020f99dc8aabf39615f2ce50f1ae6e3
Collins Omollo <omoloski5@gmail.com>
2024-06-09 23:24:24 +0300

    Updated

--- submit_contact.php
+++ submit_contact.php
@@ -1,19 +1,19 @@
 <?php
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $name = htmlspecialchars($_POST["name"]);
-//     $email = htmlspecialchars($_POST["email"]);
-//     $message = htmlspecialchars($_POST["message"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $message = htmlspecialchars($_POST["message"]);
 
     // Path to 'submissions.txt'. Ensure this path is writable by your web server.
-    // $file_path = __DIR__ . "\\submissions.txt";
+    $file_path = __DIR__ . "\\submissions.txt";
 
     // Save data to file
-//     $file = fopen($file_path, "a");
-//     fwrite($file, "Name: $name\nEmail: $email\nMessage: $message\n\n");
-//     fclose($file);
+    $file = fopen($file_path, "a");
+    fwrite($file, "Name: $name\nEmail: $email\nMessage: $message\n\n");
+    fclose($file);
 
-//     echo "Thank you, your message has been received.";
-// } else {
-//     echo "Invalid Request";
-// }
-// ?>
+    echo "Thank you, your message has been received.";
+} else {
+    echo "Invalid Request";
+}
+?>
